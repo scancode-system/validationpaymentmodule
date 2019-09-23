d0e25fcaac9e9ce6c295f2b36b731a7d46f3bb9b
Your Name <you@example.com>
2019-09-23 09:37:57 -0300

    .

--- Validator/PaymentsValidator.php
+++ Validator/PaymentsValidator.php
@@ -7,10 +7,12 @@ use Modules\Portal\Imports\ValidatorImport;
 class PaymentsValidator extends ValidatorImport
 {
 
+	protected $required = ['descricao'];
+
 	public function rule($data){
 		return  [
 			'id_condicao_pagamento' => 	'integer|min:1|unique_custom_values',
-			'descricao' => 				'required|string|max:30',
+			'descricao' => 				'filled|string|max:30',
 			'descricao_longa' => 		'string|max:100',
 			'desconto' => 				'integer|min:0|max:100',
 			'valor_minimo' => 			'numeric|min:0|regex:/^\d+(\.\d{1,2})?$/',
