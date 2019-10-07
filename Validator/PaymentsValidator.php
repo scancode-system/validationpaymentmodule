b66a84ecceb9a0da98786e204239280ea70cd694
Your Name <you@example.com>
2019-10-07 01:30:23 -0300

    .

--- Validator/PaymentsValidator.php
+++ Validator/PaymentsValidator.php
@@ -13,7 +13,7 @@ class PaymentsValidator extends ValidatorImport
 	public function rule($data){
 		return  [
 			'id_condicao_pagamento' => 	['integer', 'min:1',  new NotInCustomRule($this->chunkColumn('id_condicao_pagamento', 0, $this->row_index-2), 'Duplicado')],
-			'descricao' => 				'filled|string|max:30',
+			'descricao' => 				['filled', 'string', 'max:30', new NotInCustomRule($this->chunkColumn('descricao', 0, $this->row_index-2), 'Duplicado')],
 			'descricao_longa' => 		'string|max:100',
 			'desconto' => 				'integer|min:0|max:100',
 			'valor_minimo' => 			'numeric|min:0|regex:/^\d+(\.\d{1,2})?$/',
