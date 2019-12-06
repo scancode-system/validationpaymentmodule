3367e19cb74e73e3e4258ff3764101352d91d9b2
Your Name <you@example.com>
2019-12-06 00:35:34 -0300

    .

--- Validator/PaymentsValidator.php
+++ Validator/PaymentsValidator.php
@@ -8,7 +8,27 @@ use Modules\Portal\Rules\NotInCustomRule;
 class PaymentsValidator extends ValidatorImport
 {
 
-	protected $required = ['descricao'];
+	protected $required = ['id', 'description', 'min_value', 'discount', 'addition'];
+
+	public function rule($data){
+		return  [
+			'id' => ['integer', 'min:1', new NotInCustomRule($this->chunkColumn('id_cliente', 0, $this->row_index-2), 'Duplicado')],
+			'description' => ['filled', 'string', 'max:255', new NotInCustomRule($this->chunkColumn('description', 0, $this->row_index-2), 'Duplicado')],
+			'min_value' => 'numeric|min:0|regex:/^\d+(\.\d{1,2})?$/',
+			'discount' => 'numeric|between:0,100|regex:/^\d+(\.\d{1,2})?$/',
+			'addition' => 'numeric|between:0,100|regex:/^\d+(\.\d{1,2})?$/',
+		];
+	}
+
+	public function filterRules(){
+		return [
+			['rule' => ['min_value' => ['required', 'regex:/^(R\$)?( )?([1-9]{1}[\d]{0,2}(\.[\d]{3})*(\,[\d]{0,2})?|[1-9]{1}[\d]{0,}(\,[\d]{0,2})?|0(\,[\d]{0,2})?|(\,[\d]{1,2})?)$/']], 'filter' => 'currencyFormat'],
+			['rule' => ['discount' => ['required', 'regex:/^(R\$)?( )?([1-9]{1}[\d]{0,2}(\.[\d]{3})*(\,[\d]{0,2})?|[1-9]{1}[\d]{0,}(\,[\d]{0,2})?|0(\,[\d]{0,2})?|(\,[\d]{1,2})?)$/']], 'filter' => 'currencyFormat'],
+			['rule' => ['addition' => ['required', 'regex:/^(R\$)?( )?([1-9]{1}[\d]{0,2}(\.[\d]{3})*(\,[\d]{0,2})?|[1-9]{1}[\d]{0,}(\,[\d]{0,2})?|0(\,[\d]{0,2})?|(\,[\d]{1,2})?)$/']], 'filter' => 'currencyFormat'],
+		];
+	}
+
+	/*protected $required = ['descricao'];
 
 	public function rule($data){
 		return  [
@@ -30,5 +50,5 @@ class PaymentsValidator extends ValidatorImport
 
 	public function messages(){
 		return  [];
-	}
+	}*/
 }
